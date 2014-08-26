0183467102e2014d433336d00a8ccb327b75b273
cesarzanette <cesarzanette@gmail.com>
2014-08-25 23:44:33 -0300

    Computador
    testando

---
+++ src/Computador.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Computador {
+	
+	private int linha, coluna;
+	private Coordenada coordenada;
+	
+	public void sorteiaCoordenada(){
+		Random sorteio = new Random();
+		
+		linha = sorteio.nextInt(3);
+		coluna = sorteio.nextInt(3);
+		
+		coordenada = new Coordenada (linha,coluna);
+		
+		
+		
+	}
+	
+	
+}
